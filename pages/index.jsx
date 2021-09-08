26feefd9b3cf28f14a2faa1f74f47dc0a7e7362c
coffmanjrp <coffmanjrp@gmail.com>
2021-09-08 00:11:34 -0700

    Add more contents to index page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,37 @@
 import Head from 'next/head';
+import { useColorMode, Heading, Text, Flex, Stack } from '@chakra-ui/react';
 import Container from '../components/Container';
-import { Text } from '@chakra-ui/react';
 
 export default function Home() {
+  const { colorMode } = useColorMode();
+  const colorSecondary = {
+    light: 'gray.700',
+    dark: 'gray.400',
+  };
+
   return (
     <Container>
-      <Text>test</Text>
+      <Head>
+        <title>Home - Personal Site</title>
+      </Head>
+      <Stack
+        as="main"
+        spacing={8}
+        justifyContent="center"
+        alignItems="flex-start"
+        m="0 auto 4rem auto"
+        px={2}
+        maxWidth={700}
+      >
+        <Flex flexDirection="column" maxWidth={700}>
+          <Heading mb={2}>Hi, I&apos;m Paul Coffman Jr.</Heading>
+          <Text color={colorSecondary[colorMode]}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ante
+            nunc, finibus sit amet purus quis, congue vulputate ipsum. Phasellus
+            lobortis bibendum orci, quis imperdiet lectus imperdiet porttitor.
+          </Text>
+        </Flex>
+      </Stack>
     </Container>
   );
 }
